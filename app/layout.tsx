78e445d7c9519dfdb0c71e12a4b0c9af20ea800e
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2023-12-16 12:37:21 +0900

    ✨ layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import Header from './components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignInUrl="/" afterSignUpUrl="/onboarding">
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+    <ClerkProvider>
+      <body lang="ja">
+        <Header />
+        <main>{children}</main>
+      </body>
     </ClerkProvider>
   );
 }
